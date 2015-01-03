76f919593cfa97fbee0f69da15346c93146623e2
gillilandjj <john.j.gilliland@gmail.com>
2015-01-02 21:57:54 -0600

    Initial commit

---
+++ app/controllers/index.js
@@ -0,0 +1,17 @@
+function doClick(e) {
+    alert($.label.text);
+}
+
+function doClickRisk(e) {
+	alert("Risk");
+}
+
+function doClickRx(e) {
+	alert("Rx");
+}
+
+function doClickRef(e) {
+	alert("Ref");
+}
+
+$.index.open();
